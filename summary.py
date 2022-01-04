89f35c8623b2dd409353f5d4b3889eaad357b1cd
adrianmcphee <adrian@openunited.com>
2022-01-04 16:14:54 +0100

    Retrieving Rome contract

--- summary.py
+++ summary.py
@@ -6,5 +6,7 @@ w3 = Web3(Web3.HTTPProvider('https://rpc.moonriver.moonbeam.network'))
 w3.eth.get_block('latest')
 
 f = open('rome-abi.json')
-romeabi = json.load(f)
+rome_abi = json.load(f)
+rome_contract_address = "0x4a436073552044D5f2f49B176853ad3Ad473d9d6"
 
+rome_contract = w3.eth.contract(address=rome_contract_address, abi=rome_abi)
